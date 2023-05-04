001de1975321f40321c58bcca746dbf1e41551d8
Deepa-Senthil <deepasenthil5812@gmail.com>
2023-05-04 16:14:35 +0530

    first commit

--- src/home.jsx
+++ src/home.jsx
@@ -4,7 +4,7 @@ function Home(props) {
         <>
             <h1>From Home:</h1>
              <p>Name:{props.userDetails.userName}</p>
-             <p> Age:{props.userDetails.age}</p>
+             {/* <p> Age:{props.userDetails.age}</p> */}
              <p>Gender:{props.userDetails.gender}</p>
              <p>Location:{props. userDetails.location}</p>
         
